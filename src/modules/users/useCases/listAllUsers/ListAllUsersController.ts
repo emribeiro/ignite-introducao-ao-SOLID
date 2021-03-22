a68c7ae261a03688e50e9ff86f322d41296752f9
Eric Miranda <emribeiro@github.com>
2021-03-22 12:38:04 -0300

    Challenge implementation

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,13 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+    try{
+      const users = this.listAllUsersUseCase.execute({user_id});
+      return response.status(200).send(users);
+    }catch(err){
+      return response.status(400).send({error: err.message});
+    }
   }
 }
 
--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -9,7 +9,14 @@ class TurnUserAdminUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const foundUser = this.usersRepository.findById(user_id);
+
+    if(!foundUser){
+      throw new Error("User not Found");
+    }
+    const updatedUser = this.usersRepository.turnAdmin(foundUser);
+
+    return updatedUser;
   }
 }
 
